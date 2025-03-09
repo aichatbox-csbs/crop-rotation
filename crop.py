5e2728fee8d7e25ed28d80eb44a956e7798b35b6
aichatbox-csbs <kankalapatihashwanth@gmail.com>
2025-03-09 11:56:19 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seabon as sns
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.preprocessing import StandardScaler
